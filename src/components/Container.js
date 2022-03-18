87fb7aa32f0faf24c29d08cc0332bc791817b3ec
chriswudev <chris.wu.tiff@gmail.com>
2022-03-18 18:14:49 -0400

    minor update

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MuiContainer from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import Toolbar from 'components/Toolbar';
 import ListView from 'components/ListView';
 import GridView from 'components/GridView';
@@ -9,13 +10,26 @@ import { AppContext } from 'contexts/AppContext';
 
 export default function Container() {
   const theme = useTheme();
-  const { viewMode } = useContext(AppContext);
+  const { viewMode, todos } = useContext(AppContext);
 
   return (
     <MuiContainer maxWidth="lg">
       <Box sx={{ marginTop: theme.spacing(8) }}>
         <Toolbar />
-        {viewMode === 'list' ? <ListView /> : <GridView />}
+        {todos.length ? (
+          viewMode === 'list' ? (
+            <ListView />
+          ) : (
+            <GridView />
+          )
+        ) : (
+          <Typography
+            variant="h5"
+            sx={{ textAlign: 'center', mt: theme.spacing(8) }}
+          >
+            You don't have any ToDos yet!
+          </Typography>
+        )}
       </Box>
     </MuiContainer>
   );
